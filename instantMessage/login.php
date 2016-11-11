d95cf085e5b15aeb46637e98f57d5664821baa67
ShingJan <yuanj.shi@hotmail.com>
2016-11-11 13:24:03 +0800

    login & style sheet update

---
+++ instantMessage/login.php
@@ -0,0 +1,30 @@
+<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+<?php
+session_start ();
+function loginForm() {
+    echo '
+   <div id="loginform">
+   <form action="index.php" method="post">
+       <p>Please enter your name to continue:</p>
+       <label for="name">Name:</label>
+       <input type="text" name="name" id="name" />
+       <label for="country">Geo Info:</label>
+       <input type="text" name="country" id="country" />
+       <input type="submit" name="enter" id="enter" value="Enter" />
+   </form>
+   </div>
+   ';
+}
+loginForm();
+if (isset ( $_POST ['enter'] )) {
+    if ($_POST ['name'] != "") {
+        $_SESSION ['name'] = stripslashes ( htmlspecialchars ( $_POST ['name'] ) );
+        $fp = fopen ( "log.html", 'a' );
+        fwrite ( $fp, "<div class='msgln'><i>User " . $_SESSION ['name'] . " has joined the chat session.</i><br></div>" );
+        fclose ( $fp );
+    } else {
+        echo '<span class="error">Please type in a name</span>';
+    }
+}
+
+?>

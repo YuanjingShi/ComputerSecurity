87f6989c146c805599490d7e516fa6908783b600
ShingJan <yuanj.shi@hotmail.com>
2016-11-13 19:15:02 +0800

    login finished

--- instantMessage/login.php
+++ instantMessage/login.php
@@ -1,42 +1,25 @@
-<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
 <?php
-//session_start ();
-$test = array("Henry", "NT", "Irix", "Linux");
-function loginForm() {
-    echo '
-   <div id="loginform">
-   <form method="post">
-       <p>Please enter your name to continue:</p>
-       <label for="name">User Name:</label>
-       <input type="text" name="name" id="name" />
-       <label for="pwd">Password:</label>
-       <input type="text" name="pwd" id="pwd" />
-       <input type="submit" name="enter" id="enter" value="Enter" />
-   </form>
-   </div>
-   ';
-}
-loginForm();
+session_start ();
 $file = "user.txt";
 $fopen = fopen ( $file, "r");
 
-while(!feof($fopen)) {
-    $text[] = fgets($fopen);
+if ($fopen) {
+    $text = explode("\n", fread($fopen, filesize($file)));
 }
 fclose($fopen);
-echo in_array(henry,$text,true) ? 'It is here' : 'Sorry it is not';
+//echo in_array(henry,$text,true) ? 'It is here' : 'Sorry it is not';
 
 
 if (isset ( $_POST ['enter'] )) {
     if ($_POST ['name'] != "" && $_POST ['pwd'] != "") {
         $_SESSION ['name'] = stripslashes ( htmlspecialchars ( $_POST ['name'] ) );
         $_SESSION ['pwd'] = stripslashes ( htmlspecialchars ( $_POST ['pwd'] ) );
-        echo $_POST ['name'];
-        if(in_array($_POST ['name'], $test,true)){
+        //echo $_POST ['name'];
+        if(in_array($_SESSION ['name'], $text,true)){
             $fp = fopen ( "log.html", 'a' );
             fwrite ( $fp, "<div class='msgln'><i>User " . $_SESSION ['name'] . " has joined the chat session.</i><br></div>" );
             fclose ( $fp );
-            header("index.php");
+            header("Location: index.php");
         }else{
             echo '<span class="error">User is not registered!</span>';
         }
@@ -44,5 +27,22 @@ if (isset ( $_POST ['enter'] )) {
         echo '<span class="error">Please input sth valid</span>';
     }
 }
-
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+    </head>
+    <body>
+        <div id="loginform">
+            <form method="post">
+                <p>Please enter your name to continue:</p>
+                <label for="name">User Name:</label>
+                <input type="text" name="name" id="name" />
+                <label for="pwd">Password:</label>
+                <input type="text" name="pwd" id="pwd" />
+                <input type="submit" name="enter" id="enter" value="Enter" />
+            </form>
+        </div>
+    </body>
+    </html>

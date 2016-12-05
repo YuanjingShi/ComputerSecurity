0f8e72978811764fd4935f6ad7b674424a3a9cd1
AntyChrist <12132473d@connect.polyu.hk>
2016-12-05 11:28:14 +0800

    data encryption

---
+++ instantMessage/checkMessage.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+header("Content-Type: application/json");
+
+$grpid = $_SESSION["grpid"];
+$_SESSION["msg_no"] = $_POST["msg_no"];
+$data = json_decode(file_get_contents("groups.json"), true);
+$grp = $data[$grpid];
+
+$msgs = $grp["msgs"]; 
+if ($_SESSION["msg_no"] != count($msgs))
+{
+    $response["msg_no"] = count($msgs);
+    $response["msgs"] = array_slice($msgs, $_SESSION["msg_no"] - count($msgs));
+}
+else $response = array("msg_no" => $_SESSION["msg_no"], "msgs" => array());
+
+echo json_encode($response);
+
+?>
